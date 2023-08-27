0c9edc7848ba2be2e303535b81852a45a70d4cc8
Mateo Carmelo <analistaprogramador.b@corpacam.com.gt>
2023-08-27 09:00:41 -0600

    Service

---
+++ src/Config/Cofig.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Cofig() {
+  return (
+    <View>
+      <Text>Cofig</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
